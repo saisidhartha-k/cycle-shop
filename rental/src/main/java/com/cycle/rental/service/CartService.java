b731296938045bb9d0de3dc3e566d6185eef005e
abbagowni Anush <anush.a@prodapt.com>
2023-09-20 10:14:20 +0530

    changes in API

--- rental/src/main/java/com/cycle/rental/service/CartService.java
+++ rental/src/main/java/com/cycle/rental/service/CartService.java
@@ -3,6 +3,7 @@ package com.cycle.rental.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,26 +38,37 @@ public class CartService {
     public List<Items> addToCart(int cycleId,int quantity,String name){
         User user = userRepository.findByName(name).get();
         Optional<Cart> cart = cartRepository.findByUser(user);
+        List<Items> items = new ArrayList<>();
         Items item = new Items();
             Optional<Cycle> cycle = cycleRepository.findById(cycleId);
-            cycle.get().setNumBorrowed(quantity);
+            cycle.get().setNumBorrowed(cycle.get().getNumBorrowed()+quantity);
             cycleRepository.save(cycle.get());
             if(cycle.isPresent()){
                 item.setCycle(cycle.get());
                 item.setQuantity(quantity);
             }
         if(cart.isPresent()){
-            cart.get().getItems().add(item);
-            cartRepository.save(cart.get());    
+            items = cart.get().getItems().stream().filter(ite -> ite.getCycle().getCycleId()==cycleId).collect(Collectors.toList());
+            if(items.size()==0){
+                cart.get().getItems().add(item);
+            }
+            else{
+                cart.get().getItems().removeAll(items);
+                items.get(0).setQuantity(items.get(0).getQuantity()+quantity);
+                cart.get().getItems().addAll(items);
+            }
+            cartRepository.save(cart.get());  
+            items = cart.get().getItems();
         }
         else{
             Cart newCart = new Cart();
             newCart.setUser(user);
             newCart.getItems().add(item);
             cartRepository.save(newCart);
+            items = newCart.getItems();
         }
         
-        return cart.get().getItems();
+        return items;
     }
     public String removeFromCart(int cycleId,int quantity,String name){
         User user = userRepository.findByName(name).get();
@@ -71,7 +83,14 @@ public class CartService {
                 return "Cycle not present";
             }
         if(cart.isPresent()){
-            cart.get().getItems().remove(item);
+            cart.get().getItems().stream()
+            .peek(ite -> {
+            if (ite.getCycle().getCycleId() == cycleId) {
+            ite.getCycle().setNumBorrowed(ite.getCycle().getNumBorrowed() - quantity);
+            }
+            })
+            .filter(ite -> ite.getCycle().getCycleId() != cycleId)
+            .collect(Collectors.toList());
             return "Removed From Cart";
         }
         else{
